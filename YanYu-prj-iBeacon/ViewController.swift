557f0869f5ff99ab2b8d4fef3e45aecdb8c80e17
CHINF <chinf.work@gmail.com>
2018-02-21 09:14:45 +0800

    Initial Commit

---
+++ YanYu-prj-iBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YanYu-prj-iBeacon
+//
+//  Created by 洪權甫 on 2018/2/21.
+//  Copyright © 2018年 洪權甫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
